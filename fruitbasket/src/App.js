6e7d84d8e38cb33ecd5257bfb583e23ba0829777
raomaya <raomaya11@gmail.com>
2021-09-11 20:05:12 -0400

    add explore page

--- fruitbasket/src/App.js
+++ fruitbasket/src/App.js
@@ -4,10 +4,12 @@ import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Container from "react-bootstrap/Container";
 import Profile from "./profile";
+import Explore from "./explore";
 
 const Header = () => {
   return (
     <>
+      <div className="header">
       <Container fluid>
         <Container>
           <img src={logo} height="100" width="100" />
@@ -18,14 +20,18 @@ const Header = () => {
         </Container>
       </Container>
 
-      <Tabs justify defaultActiveKey="Discover" id="home-nav" className="mb-3">
-        <Tab eventKey="Discover" title="Discover"></Tab>
+      <Tabs justify defaultActiveKey="Explore" id="home-nav" className="mb-3">
+        <Tab eventKey="Explore" title="Explore">
+          <Explore></Explore>
+        </Tab>
+          
         <Tab eventKey="Likes" title="Likes"></Tab>
         <Tab eventKey="Messages" title="Messages"></Tab>
         <Tab eventKey="Profile" title="Profile">
           <Profile></Profile>
         </Tab>
       </Tabs>
+      </div>
     </>
   );
 };
---
+++ fruitbasket/src/explore.js
@@ -0,0 +1,13 @@
+import "./explore.css";
+const Explore = () => {
+    return(
+        <div className="app">
+            <h1> Recommended for you </h1>
+            <div className="profile">Will insert profile here</div>
+        </div>
+
+
+    );
+};
+
+export default Explore
