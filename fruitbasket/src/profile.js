c6f677d9ebc72e3c05e2523775e3a36e9dde8deb
wovy <masoncooper85@gmail.com>
2021-09-17 16:05:28 -0400

    Update profile.js

--- fruitbasket/src/profile.js
+++ fruitbasket/src/profile.js
@@ -2,7 +2,6 @@ import "./profile.css";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Container from "react-bootstrap/Container";
-import Service from "./service.js"
 
 const Profile = () => {
     return(
