843ebef603956af148ce14f093fefa668de9cd5e
wovy <masoncooper85@gmail.com>
2021-09-17 17:27:51 -0400

    firebase

--- fruitbasket/src/firebase.js
+++ fruitbasket/src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-var fireDb = firebase.initializeApp(firebaseConfig);
-
-export default fireDb.database().ref();
+const db = initializeApp(firebaseConfig);
+export default db;
