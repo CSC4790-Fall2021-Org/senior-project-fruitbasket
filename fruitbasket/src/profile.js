dac3be4e4564896f9aa4ed447c941e2b5b05d496
wovy <masoncooper85@gmail.com>
2021-09-20 16:41:31 -0400

    removed more

--- fruitbasket/src/profile.js
+++ fruitbasket/src/profile.js
@@ -2,11 +2,6 @@ import "./profile.css";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Container from "react-bootstrap/Container";
-import db from './firebase'
-import { getDatabase, ref } from "firebase/database";
-
-//const database = getDatabase(db)
-//const userRef = ref("/userid")
 
 
 const Profile = () => {
