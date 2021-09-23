b6c4adc4717060063f3794d03c534f9c41c7c772
wovy <masoncooper85@gmail.com>
2021-09-22 20:18:17 -0400

    Update explore.js

--- fruitbasket/src/explore.js
+++ fruitbasket/src/explore.js
@@ -3,6 +3,7 @@ import "./profile.css";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel"
 
 const Explore = () => {
     return(
@@ -10,13 +11,37 @@ const Explore = () => {
         <Container>
             <h1> Profile</h1>
         </Container>
-        <Tabs justify defaultActiveKey="Create" id="home-nav" className="mb-3">
-          <Tab eventKey="Create" title="Create"></Tab>
-          <Tab eventKey="Read" title="Read"></Tab>
-          <Tab eventKey="Update" title="Update"></Tab>
-          <Tab eventKey="Delete" title="Delete">
-          </Tab>
-        </Tabs>
+        
+        <Container className="block-example border border-dark">
+        <h1> Name </h1>
+        <Carousel>
+            
+            <Carousel.Item interval={1000}>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=First slide&bg=373940"
+                alt="First slide"
+                />
+            </Carousel.Item>
+            
+            <Carousel.Item interval={500}>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Second slide&bg=282c34"
+                alt="Second slide"
+                />
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                className="d-block w-100"
+                src="holder.js/800x400?text=Third slide&bg=20232a"
+                alt="Third slide"
+                />
+            </Carousel.Item>
+            </Carousel>
+            
+        </Container>
         </>
     );
 };
