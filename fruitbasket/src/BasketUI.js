71454945ca3319f9727690fc680b5f572bff613f
John Quimbo <53280993+JohnQuimbo@users.noreply.github.com>
2021-09-23 15:32:09 -0400

    converting group component into class based component

---
+++ fruitbasket/src/BasketUI.js
@@ -0,0 +1,23 @@
+import "./profile.css";
+import Card from 'react-bootstrap/Card'
+import { CardGroup } from "react-bootstrap";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import {
+    BrowserRouter as Router,
+    Link,
+    Route,
+    Switch,
+  } from 'react-router-dom';
+
+const BasketUI = () => {
+    return(
+        <div>
+            <h1>Wassup</h1>
+        </div>
+    );
+};
+
+//route to the fruit basket (write api get request that returns data of the people within the fruit basket)
+
+export default BasketUI
--- fruitbasket/src/Group.js
+++ fruitbasket/src/Group.js
@@ -8,16 +8,24 @@ import {
     Route,
     Switch,
   } from 'react-router-dom';
+import BasketUI from "./BasketUI";
+import React from 'react';
 
-const Group = () => {
-    return(
+class Group extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+      }
+    
+    render() {
+        return(
         <div>
             <Row xs={1} md={2} className="g-4">
                 {Array.from({ length: 4 }).map((_, idx) => (
                 <Col>
 
                 <Router>
-                    <Link to={'/fruitbasket/basket/test'}>
+                    <Link to={'/basket'}>
                         <Card hoverable>
                             <Card.Img variant="top" src="https://media.istockphoto.com/photos/multi_ethnic-teenagers-taking-a-self-portrait-stock-photo-picture-id1184216653?k=20&m=1184216653&s=612x612&w=0&h=T6xt_a6r-fmG0l3dzX-EHbVPQkJQeY-VX4zb7skIbPQ=" />
                             <Card.Body>
@@ -28,14 +36,21 @@ const Group = () => {
                             </Card.Body>
                         </Card>
                     </Link>
+
+                    <Switch>
+                        <Route path="/basket">
+                            <BasketUI />
+                        </Route>
+                    </Switch>
                 </Router>
                 </Col>
                     ))}
             </Row>
             <h1>Invites</h1>
         </div>
-    );
-};
+        );
+        };
+    }
 
 //route to the fruit basket (write api get request that returns data of the people within the fruit basket)
 
