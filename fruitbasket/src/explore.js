761f48e29530565622b91f7ffbfebee29b1f91f7
wovy <masoncooper85@gmail.com>
2021-09-22 22:26:24 -0400

    bleh

--- fruitbasket/src/explore.js
+++ fruitbasket/src/explore.js
@@ -16,8 +16,19 @@ const Explore = () => {
 
         <Container className="d-flex justify-content-around mt-5 profileheader">
          John Smith | Age 79 | Villanova, PA
-        <Button className="rounded-pill" size="lg"> PASS </Button>
-        <Button className="rounded-pill" size="lg"> LIKE </Button>
+                <Button className="like-button btn-outline-dark bg-transparent">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                    </svg>
+                PASS
+              </Button>
+
+                <Button className="like-button btn-outline-danger bg-transparent">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                    </svg>
+                LIKE
+              </Button>
         </Container>
 
         <Carousel className="explorecarousel m-5">
