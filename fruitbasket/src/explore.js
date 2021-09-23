1c53457975d726166fc73c840965ec6f6e9f6725
wovy <masoncooper85@gmail.com>
2021-09-22 21:57:23 -0400

    explore

--- fruitbasket/src/explore.js
+++ fruitbasket/src/explore.js
@@ -12,18 +12,18 @@ const Explore = () => {
         
         <Container className="block-example border border-dark rounded shadow-lg">
 
-        <Container className="d-flex justify-content-around">
+        <Container className="d-flex justify-content-around mt-5">
          First Name Last Name
-        <Button className="rounded-pill"> PASS </Button>
-        <Button className="rounded-pill"> LIKE </Button>
+        <Button className="rounded-pill" size="lg"> PASS </Button>
+        <Button className="rounded-pill" size="lg"> LIKE </Button>
         </Container>
 
-        <Carousel>
+        <Carousel className="explorecarousel m-5">
             
             <Carousel.Item interval={1000}>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
+                src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
                 alt="First slide"
                 />
             </Carousel.Item>
@@ -31,7 +31,7 @@ const Explore = () => {
             <Carousel.Item interval={500}>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
+                src="https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg"
                 alt="Second slide"
                 />
             </Carousel.Item>
@@ -39,13 +39,21 @@ const Explore = () => {
             <Carousel.Item>
                 <img
                 className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
+                src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4="
                 alt="Third slide"
                 />
             </Carousel.Item>
             </Carousel>
 
+        <Container className="bio p-5">
+        <h1>Bio</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </Container>
+
+        </Container>
+        
+
+        
         </>
     );
 };
