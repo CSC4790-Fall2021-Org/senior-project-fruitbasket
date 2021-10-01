1ef86357d656ad894ff3dfd71ec5a89e14bfd535
John Quimbo <johnanthony.quimbo@gmail.com>
2021-10-01 18:24:26 -0400

    added Users Model

---
+++ fruit-basket-backend/fruit-basket-backend/Models/Users.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Users
+{
+	public string User_ID { get; set; }
+
+	public string UserName { get; set; }
+
+	public string First_Name { get; set; }
+
+	public string Last_Name { get; set; }
+
+	public string Bio { get; set; }
+
+	public string Password { get; set; }
+
+	public string Email { get; set; }
+
+	public string Phone_Number { get; set; }
+
+	public DateTime Date_Of_Birth { get; set; }
+
+	public int Number_Of_Matches { get; set; }
+
+	public string City { get; set; }
+		
+	public int Age { get; set; }
+
+	public bool Interest_In_Basket { get; set; }
+
+	public string Preference_ID { get; set; }
+}
