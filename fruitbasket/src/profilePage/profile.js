6fbbc78947084e41d455007ddf794a9591de65ca
wovy <masoncooper85@gmail.com>
2021-10-20 18:38:32 -0400

    Update profile.js

--- fruitbasket/src/profilePage/profile.js
+++ fruitbasket/src/profilePage/profile.js
@@ -42,60 +42,7 @@ class Profile extends Component {
     const { profiles } = this.state;
     return (
       <>
-        <Tabs
-          justify
-          defaultActiveKey="Create"
-          id="home-nav"
-          className="mb-3 navbarmain"
-        >
-          <Tab eventKey="Create" title="Create" className="createtab">
-            <Form>
-              <Form.Group className="mb-3">
-                <Form.Label>Name</Form.Label>
-                <Form.Control placeholder="Enter test name" />
-                <Form.Text className="text-muted">
-                  Test add name
-                </Form.Text>
-              </Form.Group>
-
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Tab>
-
-          <Tab eventKey="Read" title="Read">
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>name</th>
-                  <th>address</th>
-                  <th>company</th>
-                  <th>email</th>
-                  <th>phone</th>
-                  <th>username</th>
-                  <th>website</th>
-                </tr>
-              </thead>
-              <tbody>
-                {profiles.map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>{user.address.city}</td>
-                    <td>{user.company.name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.phone}</td>
-                    <td>{user.username}</td>
-                    <td>{user.website}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Tab>
-          <Tab eventKey="Update" title="Update"></Tab>
-
-          <Tab eventKey="Delete" title="Delete"></Tab>
-        </Tabs>
+      
       </>
     );
   }
