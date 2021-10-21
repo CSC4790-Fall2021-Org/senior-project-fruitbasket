b7d5750a693cd967d5bf79701b35886cebf95389
raomaya <55894629+raomaya@users.noreply.github.com>
2021-10-21 11:52:58 -0700

    Update ExploreIndivual.js

--- fruitbasket/src/explorePage/ExploreIndivual.js
+++ fruitbasket/src/explorePage/ExploreIndivual.js
@@ -41,7 +41,7 @@ class ExploreIndivual extends React.Component {
         <Container className="block-example border border-dark rounded shadow-lg">
 
         <Container className="d-flex justify-content-around mt-5 profileheader">
-         {this.props.state.name} | {this.props.state.age} | {this.props.state.city}
+         {this.props.name} | {this.props.age} | {this.props.city}
                 <Button className="like-button btn-outline-dark bg-transparent" onClick={this.props.handleChange}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
