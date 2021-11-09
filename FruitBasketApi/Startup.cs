5da3f4e66989abd0ab74059f5f2eebb2384fa481
wovy <masoncooper85@gmail.com>
2021-11-09 00:37:14 -0500

    setting up jwt

--- FruitBasketApi/Startup.cs
+++ FruitBasketApi/Startup.cs
@@ -13,6 +13,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using FruitBasketApi.Models;
+using FruitBasketApi.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FruitBasketApi
 {
@@ -28,12 +32,35 @@ namespace FruitBasketApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //read jwt secret token from appsettings, after this can be used throughout application
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
 
             services.AddControllers();
 
             services.AddDbContext<FruitBasketContext>(opt =>
                                                opt.UseSqlServer(Configuration.GetConnectionString("Default")));
-            
+
+
+            //add authentication
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(jwt => {
+                var key = Encoding.ASCII.GetBytes(Configuration["JwtConfig:Secret"]);
+                jwt.SaveToken = true;
+                jwt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    RequireExpirationTime = false
+                };
+            });
+
             //services.AddSwaggerGen(c =>
             //{
             //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FruitBasketApi", Version = "v1" });
