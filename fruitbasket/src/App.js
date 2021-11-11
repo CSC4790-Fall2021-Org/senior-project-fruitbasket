510d21edd3e0e0aee6085eed2fa9aedbc7afcdf1
wovy <masoncooper85@gmail.com>
2021-11-10 23:33:50 -0500

    Update App.js

--- fruitbasket/src/App.js
+++ fruitbasket/src/App.js
@@ -12,31 +12,29 @@ function App() {
         <Container className="text-center">
           <img src={logo} height="100" width="100" />
         </Container>
-
         <Container>
-          <h1 className="text-center"> FRUITBASKET </h1>
-        </Container>
-      </Container>
+          <h1 className="text-center"> FRUITBASKET </h1>{" "}
+        </Container>{" "}
+      </Container>{" "}
       <div>
         <ul>
           <li>
-            <Link to="/">login</Link>
-          </li>
+            <Link to="/"> login </Link>{" "}
+          </li>{" "}
           <li>
-            <Link to="/app">app</Link>
-          </li>
+            <Link to="/app"> app </Link>{" "}
+          </li>{" "}
         </ul>
-
         <hr />
         <Switch>
           <Route exact path="/">
             <Login />
-          </Route>
+          </Route>{" "}
           <Route path="/app">
             <Header />
-          </Route>
-        </Switch>
-      </div>
+          </Route>{" "}
+        </Switch>{" "}
+      </div>{" "}
     </Router>
   );
 }
