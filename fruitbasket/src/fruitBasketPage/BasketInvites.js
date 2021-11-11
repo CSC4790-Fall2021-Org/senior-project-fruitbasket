5db2bfff827eacbd6a3f8479cc62f1961a9156a9
John Quimbo <53280993+JohnQuimbo@users.noreply.github.com>
2021-11-11 12:36:25 -0500

    added yes or no to basket join requests

--- fruitbasket/src/fruitBasketPage/BasketInvites.js
+++ fruitbasket/src/fruitBasketPage/BasketInvites.js
@@ -31,7 +31,7 @@ function BasketInvites() {
             
             <Container className="text-right p-3">
                 <Row >
-                    <Col xs={10} className="block-example border border-primary">
+                    <Col xs={9} className="block-example border border-primary">
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -42,19 +42,21 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
                 </Row>
 
                 <Row >
-                    <Col xs={10} className="block-example border border-secondary">
+                    <Col xs={9} className="block-example border border-secondary">
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -65,19 +67,21 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
                 </Row>
 
                 <Row>
-                    <Col xs={10} className="block-example border border-success">
+                    <Col xs={9} className="block-example border border-success">
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -88,19 +92,21 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
                 </Row>
 
                 <Row >
-                    <Col xs={10} className="block-example border border-danger">
+                    <Col xs={9} className="block-example border border-danger">
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -111,19 +117,21 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
                 </Row>
 
                 <Row >
-                    <Col className="block-example border border-warning" xs={10}>
+                    <Col className="block-example border border-warning" xs={9}>
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -134,19 +142,21 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
                 </Row>
                 
                 <Row >
-                    <Col className="block-example border border-info" xs={10}>
+                    <Col className="block-example border border-info" xs={9}>
                         <Conversation name="Patrik" lastSenderName="Patrik" info="You up?" onClick={ () => alert("Pretend this is showing the profile you clicked on")}>
                         <Avatar src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/shrek-forever-after-1587549453.jpg?crop=0.676xw:0.901xh;0.0969xw,0&resize=480:*" name="Patrik" />
                         </Conversation>
@@ -157,12 +167,14 @@ function BasketInvites() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                             <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                             </svg>
+                            No
                             </Button>
 
                             <Button className="like-button btn-outline-danger bg-transparent" /*onClick={this.handleChange}*/>
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                             </svg>
+                            Yes
                             </Button>
                         </ButtonGroup>
                     </Col>
