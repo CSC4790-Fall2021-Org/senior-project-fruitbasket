5a36e54d65958f4bfc24d1b49d767f6cfaae557a
wovy <masoncooper85@gmail.com>
2021-11-10 22:39:37 -0500

    Update App.js

--- fruitbasket/src/App.js
+++ fruitbasket/src/App.js
@@ -7,49 +7,45 @@ import Profile from "./profilePage/profile";
 import Explore from "./explorePage/explore";
 import Group from "./Group";
 import Likes from "./Likes";
-import Messages from "./Messages"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Messages from "./Messages";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+const Login = () => {
+  return (
+    <>
+      <p>login</p>
+    </>
+  );
+};
 
 const Header = () => {
   return (
     <>
       <div className="header">
-      <Container fluid>
-        <Container className="text-center">
-          <img src={logo} height="100" width="100" />
-        </Container>
+        <Tabs
+          justify
+          defaultActiveKey="Explore"
+          id="home-nav"
+          className="mb-3 navbarmain"
+        >
+          <Tab eventKey="Explore" title="Explore" className="exploretab">
+            <Explore></Explore>
+          </Tab>
 
-        <Container>
-          <h1 className="text-center"> FRUITBASKET </h1>
-        </Container>
-      </Container>
+          <Tab eventKey="Likes" title="Likes">
+            <Likes></Likes>
+          </Tab>
 
-      <Tabs justify defaultActiveKey="Explore" id="home-nav" className="mb-3 navbarmain">
-        <Tab eventKey="Explore" title="Explore" className="exploretab">
-          <Explore></Explore>
-        </Tab>
-          
-        <Tab eventKey="Likes" title="Likes">
-          <Likes></Likes>
-        </Tab>
-          
-        <Tab eventKey="Messages" title="Messages">
-          <Messages></Messages>
-        </Tab>
-        <Tab eventKey="Fruit Baskets" title="Fruit Baskets">
-          <Group></Group>
-        </Tab>
-        <Tab eventKey="Profile" title="Profile">
-          <Profile></Profile>
-        </Tab>
-        
-      </Tabs>
+          <Tab eventKey="Messages" title="Messages">
+            <Messages></Messages>
+          </Tab>
+          <Tab eventKey="Fruit Baskets" title="Fruit Baskets">
+            <Group></Group>
+          </Tab>
+          <Tab eventKey="Profile" title="Profile">
+            <Profile></Profile>
+          </Tab>
+        </Tabs>
       </div>
     </>
   );
@@ -57,9 +53,38 @@ const Header = () => {
 
 function App() {
   return (
-    <Container className="App">
-        <Header />
-    </Container>
+<Router>
+<Container fluid>
+          <Container className="text-center">
+            <img src={logo} height="100" width="100" />
+          </Container>
+
+          <Container>
+            <h1 className="text-center"> FRUITBASKET </h1>
+          </Container>
+        </Container>
+<div>
+  <ul>
+    <li>
+      <Link to="/">login</Link>
+    </li>
+    <li>
+      <Link to="/app">app</Link>
+    </li>
+
+  </ul>
+
+  <hr />
+  <Switch>
+    <Route exact path="/">
+      <Login />
+    </Route>
+    <Route path="/app">
+      <Header />
+    </Route>
+  </Switch>
+</div>
+</Router>
   );
 }
 
