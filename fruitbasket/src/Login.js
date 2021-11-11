f301511ede796e7f9365089f9b45771c83b60951
wovy <masoncooper85@gmail.com>
2021-11-11 17:19:05 -0500

    Update Login.js

--- fruitbasket/src/Login.js
+++ fruitbasket/src/Login.js
@@ -1,6 +1,7 @@
 import Form from "react-bootstrap/Form";
 import React, { Component } from "react";
 import Card from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import axios from "axios";
@@ -31,6 +32,7 @@ class Login extends Component {
   };
 
   postRequest = () => {
+    console.log("asdf")
     axios
       .post(
         "https://fruitbasketapi20211021012825.azurewebsites.net/api/users",
@@ -100,93 +102,128 @@ class Login extends Component {
             <div class="col-md-1"></div>
             <div class="col-md-3">
               <h1 className="text-center">Create New User</h1>
-                  <form onSubmit={this.postRequest}>
+              <form onSubmit={this.postRequest}>
+                <Row>
                   <label for="users_ID">users_ID</label>
-                    <input
-                      type="text"
-                      name="users_ID"
-                      value={users_ID}
-                      onChange={this.onChange}
-                    />
-                    <label for="first_Name">first_Name</label>
-                    <input
-                      type="text"
-                      name="first_Name"
-                      value={first_Name}
-                      onChange={this.onChange}
-                    />
-                    <label for="last_Name">last_Name</label>
-                    <input
-                      type="text"
-                      name="last_Name"
-                      value={last_Name}
-                      onChange={this.onChange}
-                    />
-                    <label for="bio">bio</label>
-                     <input
-                      type="text"
-                      name="bio"
-                      value={bio}
-                      onChange={this.onChange}
-                    />
-                    <label for="user_Password">user_Password</label>
-                     <input
-                      type="text"
-                      name="user_Password"
-                      value={user_Password}
-                      onChange={this.onChange}
-                    />
-                    <label for="user_Email">user_Email</label>
-                     <input
-                      type="text"
-                      name="user_Email"
-                      value={user_Email}
-                      onChange={this.onChange}
-                    />
-                    <label for="phone_Number">phone_Number</label>
-                     <input
-                      type="text"
-                      name="phone_Number"
-                      value={phone_Number}
-                      onChange={this.onChange}
-                    />
-                    <label for="date_Of_Birth">date_Of_Birth</label>
-                     <input
-                      type="text"
-                      name="date_Of_Birth"
-                      value={date_Of_Birth}
-                      onChange={this.onChange}
-                    />
-                    <label for="number_Of_Matches">number_Of_Matches</label>
-                     <input
-                      type="text"
-                      name="number_Of_Matches"
-                      value={number_Of_Matches}
-                      onChange={this.onChange}
-                    />
-                    <label for="number_Of_Baskets">number_Of_Baskets</label>
-                     <input
-                      type="text"
-                      name="number_Of_Baskets"
-                      value={number_Of_Baskets}
-                      onChange={this.onChange}
-                    />
-                    <label for="city">city</label>
-                     <input
-                      type="text"
-                      name="city"
-                      value={city}
-                      onChange={this.onChange}
-                    />
-                    <label for="age">age</label>
-                     <input
-                      type="text"
-                      name="age"
-                      value={age}
-                      onChange={this.onChange}
-                    />
-
-                  </form>
+                  <input
+                    type="text"
+                    name="users_ID"
+                    value={users_ID}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="usersName">userName</label>
+                  <input
+                    type="text"
+                    name="userName"
+                    value={userName}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="first_Name">first_Name</label>
+                  <input
+                    type="text"
+                    name="first_Name"
+                    value={first_Name}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="last_Name">last_Name</label>
+                  <input
+                    type="text"
+                    name="last_Name"
+                    value={last_Name}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="bio">bio</label>
+                  <input
+                    type="text"
+                    name="bio"
+                    value={bio}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="user_Password">user_Password</label>
+                  <input
+                    type="text"
+                    name="user_Password"
+                    value={user_Password}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="user_Email">user_Email</label>
+                  <input
+                    type="text"
+                    name="user_Email"
+                    value={user_Email}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="phone_Number">phone_Number</label>
+                  <input
+                    type="text"
+                    name="phone_Number"
+                    value={phone_Number}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="date_Of_Birth">date_Of_Birth</label>
+                  <input
+                    type="text"
+                    name="date_Of_Birth"
+                    value={date_Of_Birth}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="number_Of_Matches">number_Of_Matches</label>
+                  <input
+                    type="text"
+                    name="number_Of_Matches"
+                    value={number_Of_Matches}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="number_Of_Baskets">number_Of_Baskets</label>
+                  <input
+                    type="text"
+                    name="number_Of_Baskets"
+                    value={number_Of_Baskets}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="city">city</label>
+                  <input
+                    type="text"
+                    name="city"
+                    value={city}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                  <label for="age">age</label>
+                  <input
+                    type="text"
+                    name="age"
+                    value={age}
+                    onChange={this.onChange}
+                  />
+                </Row>
+                <Row>
+                <button type="submit">Submit</button>
+                </Row>
+              </form>
             </div>
           </div>
         </div>
