20182d25adbbc8062be66545c4eabb34be8e0e18
raomaya <raomaya11@gmail.com>
2021-12-02 11:44:36 -0500

    slider

---
+++ fruitbasket/src/explorePage/AgeSlider.js
@@ -0,0 +1,23 @@
+import "./AgeSlider.css";
+import React from "react";
+
+const AgeSlider = () => {
+
+    return (
+        <>
+            <div class="slidecontainer">
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+            </div>
+        </>
+      );
+
+
+
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+
+  
+
+};
+    
+export default AgeSlider;
